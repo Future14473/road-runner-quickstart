10d1d227e23bece08595b4cd9519e6b65bcb50f3
kyleng <ng.kyle101@gmail.com>
2021-01-31 12:20:30 -0800

    Trying to fix PID Velocity from straighttest overshooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -32,7 +32,7 @@ public class DriveConstants {
      * from DriveVelocityPIDTuner.
      */
     public static final boolean RUN_USING_ENCODER = true;
-    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(25, 0, 10, 0);
+    public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(23, 0, 10, 0);
 
     // old f value: getMotorVelocityF(MAX_RPM / 60 * TICKS_PER_REV)
 

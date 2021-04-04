16cb855fff96f34a0007eb5313aa4311af3f405c
kyleng <ng.kyle101@gmail.com>
2021-04-04 12:07:17 -0700

    Trying to fix shooter velocities

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ourOpModes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ourOpModes/Teleop.java
@@ -12,6 +12,7 @@ import org.firstinspires.ftc.teamcode.ourMovementLib.Follower;
 import org.firstinspires.ftc.teamcode.ourOpModes.resources.IMU;
 import org.firstinspires.ftc.teamcode.ourOpModes.resources.RotationUtil;
 import org.firstinspires.ftc.teamcode.ourOpModes.robotParts.RingCollector;
+import org.firstinspires.ftc.teamcode.ourOpModes.robotParts.Shooter;
 import org.firstinspires.ftc.teamcode.ourOpModes.robotParts.ShooterFlicker;
 import org.firstinspires.ftc.teamcode.ourOpModes.robotParts.Wobble_Arm;
 
@@ -26,7 +27,7 @@ public class Teleop extends LinearOpMode
     // Declare OpMode members.
     //Mecanum MecanumDrive;
 
-    DcMotorEx shooter;
+
     IMU imu;
     double headingZero = 0;
     double shooterVel = 1400;
@@ -42,7 +43,7 @@ public class Teleop extends LinearOpMode
         ShooterFlicker flicker = new ShooterFlicker(hardwareMap, this, telemetry);
 
 
-        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+        Shooter shooter = new Shooter(hardwareMap);
 
         telemetry.addData("Status", "Initialized");
 
@@ -57,6 +58,8 @@ public class Teleop extends LinearOpMode
 
         waitForStart();
 
+
+
         while (opModeIsActive()){
 
             double y = -gamepad1.right_stick_y;
@@ -67,12 +70,13 @@ public class Teleop extends LinearOpMode
             double magnitude = Math.hypot(gamepad1.left_stick_y, gamepad1.left_stick_x);
             double turnPwr = RotationUtil.turnLeftOrRight(imu.getHeading(), targetDir + headingZero, Math.PI * 2);
 
+
             if(gamepad2.dpad_left){
-                shooterVel = 1400;
+                shooter.setHighGoalSpeed();
             }
 
             if(gamepad2.dpad_right){
-                shooterVel = 1200;
+                shooter.setPowerShotSpeed();
             }
 
             if (! (gamepad1.right_trigger > 0 || gamepad1.left_trigger > 0) ){
@@ -87,11 +91,7 @@ public class Teleop extends LinearOpMode
 
             follower.DRIVE(y, x, (magnitude > 0.5 && Math.abs(turnPwr) > 0.08) ? -turnPwr/2 : 0);
 
-            // keep the velocity at 1710 at all times
-            if(shooter.getVelocity() < shooterVel)
-            {
-                shooter.setVelocity(200); //todo check if this actually works
-            }
+
 
 
             if (gamepad2.left_trigger != 0){ ringCollector.collect(); }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ourOpModes/robotParts/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ourOpModes/robotParts/Shooter.java
@@ -1,5 +1,32 @@
 package org.firstinspires.ftc.teamcode.ourOpModes.robotParts;
 
-public class Shooter {
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+public class Shooter {
+    DcMotorEx shooter_motor, shooter_encoder;
+    int powerShotSpeed = 1400, highGoalSpeed = 1600;
+
+    public Shooter(HardwareMap hardwareMap){
+        shooter_motor = hardwareMap.get(DcMotorEx.class, "shooter");
+        shooter_encoder = hardwareMap.get(DcMotorEx.class, "backLeft");
+    }
+
+    public double getShooterVelocity(){
+        return shooter_encoder.getVelocity();
+    }
+
+    public void setHighGoalSpeed(){
+        if(shooter_encoder.getVelocity() < highGoalSpeed)
+        {
+            shooter_motor.setVelocity(200); //todo check if this actually works
+        }
+    }
+
+    public void setPowerShotSpeed(){
+        if(shooter_encoder.getVelocity() < powerShotSpeed)
+        {
+            shooter_motor.setVelocity(200); //todo check if this actually works
+        }
+    }
 }

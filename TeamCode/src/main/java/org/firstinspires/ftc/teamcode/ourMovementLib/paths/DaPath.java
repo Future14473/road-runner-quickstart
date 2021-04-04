30dfc8264818516ba64bb342e75fa499e5b8e699
Austin Shih <austin2shih@yahoo.com>
2021-04-04 15:44:34 -0700

    temp, just need to get more files in

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ourMovementLib/paths/DaPath.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.ourMovementLib.paths;
+
+import org.firstinspires.ftc.teamcode.ourMovementLib.PathPoint;
+
+public class DaPath {
+    public static PathPoint origin = new PathPoint(0,0,0);
+
+    //test points
+    public static PathPoint forward = new PathPoint(0,10,0);
+    public static PathPoint strafe = new PathPoint(10,0,0);
+    public static PathPoint turn = new PathPoint(0,0, Math.toRadians(90));
+}

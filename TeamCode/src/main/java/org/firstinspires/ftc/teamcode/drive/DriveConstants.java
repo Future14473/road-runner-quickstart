f4981f6b0a66a86b65e707f1287da8a0e922cbaf
Austin Shih <austin2shih@yahoo.com>
2021-04-05 15:31:24 -0700

    RR stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -66,7 +66,7 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = 840.7558;
     public static double MAX_ANG_ACCEL = Math.toRadians(60);
 
 

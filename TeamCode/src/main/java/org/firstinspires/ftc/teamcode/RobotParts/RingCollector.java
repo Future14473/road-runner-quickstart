b0e8bcc98ffac11ea5dc681afebc0e8457b3e952
kyleng <ng.kyle101@gmail.com>
2021-04-24 00:20:30 -0700

    Adding second motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/RingCollector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/RingCollector.java
@@ -1,16 +1,20 @@
 package org.firstinspires.ftc.teamcode.RobotParts;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class RingCollector {
-    DcMotor intake, taco;
+    DcMotor intake1,intake2, taco;
     public RingCollector(HardwareMap hardwareMap){
-        intake = hardwareMap.get(DcMotor.class, "intake");
+        intake1 = hardwareMap.get(DcMotor.class, "intake");
+        intake2 = hardwareMap.get(DcMotor.class, "intake2");
+        intake2.setDirection(DcMotorSimple.Direction.REVERSE);
         taco = hardwareMap.get(DcMotor.class, "taco");
     }
     public void collect(double speed){
-        intake.setPower(speed);
+        intake1.setPower(speed);
+        intake2.setPower(speed);
         taco.setPower(speed);
     }
 

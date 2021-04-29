d1054b927d2bc6ebaae49b6dc382beeef82a5a1f
kyleng <ng.kyle101@gmail.com>
2021-04-28 21:20:12 -0700

    Wobble arm down up servos tuned

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Wobble_Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Wobble_Arm.java
@@ -2,12 +2,7 @@ package org.firstinspires.ftc.teamcode.RobotParts;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.hardware.PIDCoefficients;
-import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.ourOpModes.resources.Timing;
@@ -15,12 +10,14 @@ import org.firstinspires.ftc.teamcode.ourOpModes.resources.Timing;
 @Config
 public class Wobble_Arm {
 //    DcMotorEx angler;
-    Servo angler0, angler1;
+    Servo anglerFront, anglerBack;
 
     Servo gripper;
     Timing timer;
 
-    public static int upPos = -190, downPos = -360;
+    public static double upPos = 0.4, downPos = 0.75, homePos = 0;
+//    public static int upPosBack = 0, downPosBack = 1, homePosBack = -1;
+
     public static double grabPos = 0.3, unGrabPos = 1;
 
     public Wobble_Arm(HardwareMap hardwareMap, LinearOpMode opMode){
@@ -30,8 +27,9 @@ public class Wobble_Arm {
 //        angler.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, new PIDFCoefficients(15, 3, 6, 0));
 //        angler.setMode(DcMotorEx.RunMode.RUN_TO_POSITION);
 
-        angler0 = hardwareMap.get(Servo.class, "angler0");
-        angler1 = hardwareMap.get(Servo.class, "angler1");
+        anglerFront = hardwareMap.get(Servo.class, "anglerFront");
+        anglerBack = hardwareMap.get(Servo.class, "anglerBack");
+        anglerBack.setDirection(Servo.Direction.REVERSE);
 
         gripper = hardwareMap.get(Servo.class, "wobble_gripper");
         timer = new Timing(opMode);
@@ -41,17 +39,23 @@ public class Wobble_Arm {
 //        angler.setTargetPosition(upPos);
 //        angler.setPower(0.6);
         //angler0.setPosition();
+        anglerBack.setPosition(upPos);
+        anglerFront.setPosition(upPos);
     }
     // 0.44 goes all the way into the robot
 
     public void home(){
 //        angler.setTargetPosition(-50);
 //        angler.setPower(0.6);
+        anglerBack.setPosition(homePos);
+        anglerFront.setPosition(homePos);
     }
 
     public void down(){
 //        angler.setTargetPosition(downPos);
 //        angler.setPower(0.6);
+        anglerBack.setPosition(downPos);
+        anglerFront.setPosition(downPos);
     }
 
     public void automaticReleaseWobble(){

0e3554e2a58d95bd10c8859aeb13e8eac43348fa
UpsideDownTurtle <theajaybodla@gmail.com>
2021-08-20 20:15:06 -0700

    Updated Gear Ratio

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -46,7 +46,7 @@ public class DriveConstants {
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
     public static double WHEEL_RADIUS = 1.9685; // in
-    public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
+    public static double GEAR_RATIO = 1 * 1.0195758564; // output (wheel) speed / input (motor) speed
     public static double TRACK_WIDTH = 14; // in
 
     /*

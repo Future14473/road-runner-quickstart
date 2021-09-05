9d9bb2b69889de7d26bd5f17e80970f50672a8ed
kyleng <ng.kyle101@gmail.com>
2021-09-04 21:37:29 -0700

    Added correct versions from Intellij

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/gradde/mathUtils/StandardDev.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/gradde/mathUtils/StandardDev.java
@@ -2,12 +2,10 @@ package org.firstinspires.ftc.teamcode.drive.opmode.gradde.mathUtils;
 
 public class StandardDev {
     public static double getStandardDev(double[] loss){
-        double avg = getAverageArray(loss);
-
         double sum = 0;
 
         for (double val : loss){
-            sum += Math.pow( val - avg , 2.0);
+            sum += Math.pow( val - 0 , 2.0);
         }
         return Math.sqrt(sum/(loss.length-1));
     }

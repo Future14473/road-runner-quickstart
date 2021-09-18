63df6d4d70bd6a739f8da8a99b0a0a78ed6c9772
kyleng <ng.kyle101@gmail.com>
2021-09-18 14:40:26 -0700

    Normal Drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TestThreeHour.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.drive.opmode;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+@TeleOp(group = "drive")
+public class TestThreeHour extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        drive.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            drive.setWeightedDrivePower(
+                    new Pose2d(
+                            -gamepad1.left_stick_y,
+                            -gamepad1.left_stick_x,
+                            -gamepad1.right_stick_x
+                    )
+            );
+
+            drive.update();
+
+            Pose2d poseEstimate = drive.getPoseEstimate();
+            Pose2d lastError = drive.getLastError();
+            telemetry.addData("x", poseEstimate.getX());
+            telemetry.addData("y", poseEstimate.getY());
+            telemetry.addData("heading", poseEstimate.getHeading());
+
+            telemetry.addData("xError", lastError.getX());
+            telemetry.addData("yError", lastError.getY());
+            telemetry.addData("headingError (deg)", Math.toDegrees(lastError.getHeading()));
+            telemetry.update();
+        }
+    }
+}

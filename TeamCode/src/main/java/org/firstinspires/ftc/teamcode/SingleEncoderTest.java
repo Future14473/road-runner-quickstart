b41d2d130f567127a815f92d8b51976f6268b16b
kyleng <ng.kyle101@gmail.com>
2021-11-07 12:41:45 -0800

    1 Motors Not Working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleEncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleEncoderTest.java
@@ -12,14 +12,17 @@ public class SingleEncoderTest extends LinearOpMode {
     public static int velocity = 1220;
     public static int deltaMovement = 100;
     DcMotorEx linearSlide;
+    DcMotorEx linearSlide2;
     @Override
     public void runOpMode() throws InterruptedException {
 
         linearSlide = hardwareMap.get(DcMotorEx.class, "linearSlide");
+        linearSlide2 = hardwareMap.get(DcMotorEx.class, "linearSlide2");
 //        DcMotorEx linearSlide2 = hardwareMap.get(DcMotorEx.class, "linearSlide2");
 
 
         linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         //linearSlide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         //weird thing since it is giving an error for needing to set a position before you start
@@ -29,15 +32,22 @@ public class SingleEncoderTest extends LinearOpMode {
 //        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 //        linearSlide2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         telemetry.addData("Current Position", linearSlide.getCurrentPosition());
+        telemetry.addData("Current Position", linearSlide2.getCurrentPosition());
         telemetry.update();
         waitForStart();
         linearSlide.setTargetPosition(-287);
+        linearSlide2.setTargetPosition(-290);
         linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        linearSlide2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         linearSlide.setVelocity(200);
+        linearSlide2.setVelocity(200);
         while(opModeIsActive()){
             telemetry.addData("velocity", linearSlide.getVelocity());
             telemetry.addData("position", linearSlide.getCurrentPosition());
             telemetry.addData("is at target", !linearSlide.isBusy());
+            telemetry.addData("velocity", linearSlide2.getVelocity());
+            telemetry.addData("position", linearSlide2.getCurrentPosition());
+            telemetry.addData("is at target", !linearSlide2.isBusy());
             telemetry.update();
 
         }

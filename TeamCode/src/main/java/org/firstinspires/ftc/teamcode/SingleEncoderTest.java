61db5bb421b91a96703280dd9d88b294ea6f49c1
kyleng <ng.kyle101@gmail.com>
2021-11-07 12:40:02 -0800

    1 Motor works for Single Encoder Test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SingleEncoderTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(group = "linearSlidesTest")
+@Config
+public class SingleEncoderTest extends LinearOpMode {
+    public static int velocity = 1220;
+    public static int deltaMovement = 100;
+    DcMotorEx linearSlide;
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        linearSlide = hardwareMap.get(DcMotorEx.class, "linearSlide");
+//        DcMotorEx linearSlide2 = hardwareMap.get(DcMotorEx.class, "linearSlide2");
+
+
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //linearSlide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //weird thing since it is giving an error for needing to set a position before you start
+//        linearSlide.setTargetPosition(0);
+//        linearSlide.setTargetPosition(0);
+
+//        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        linearSlide2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        telemetry.addData("Current Position", linearSlide.getCurrentPosition());
+        telemetry.update();
+        waitForStart();
+        linearSlide.setTargetPosition(-287);
+        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        linearSlide.setVelocity(200);
+        while(opModeIsActive()){
+            telemetry.addData("velocity", linearSlide.getVelocity());
+            telemetry.addData("position", linearSlide.getCurrentPosition());
+            telemetry.addData("is at target", !linearSlide.isBusy());
+            telemetry.update();
+
+        }
+
+
+
+//            linearSlide.setVelocity(0);
+
+        }
+    }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearSlidesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/linearSlidesTest.java
@@ -1,61 +1,61 @@
-//package org.firstinspires.ftc.teamcode;
-//
-//import com.acmerobotics.dashboard.config.Config;
-//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-//import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-//import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorEx;
-//
-//@TeleOp(group = "linearSlidesTest")
-//@Config
-//public class linearSlidesTest extends LinearOpMode {
-//    public static int velocity = 1220;
-//    public static int deltaMovement = 100;
-//
-//    @Override
-//    public void runOpMode() throws InterruptedException {
-//        DcMotorEx linearSlide = hardwareMap.get(DcMotorEx.class, "linearSlide");
-//        DcMotorEx linearSlide2 = hardwareMap.get(DcMotorEx.class, "linearSlide2");
-//
-//
-//        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//        linearSlide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-//
-//        //weird thing since it is giving an error for needing to set a position before you start
-////        linearSlide.setTargetPosition(0);
-////        linearSlide.setTargetPosition(0);
-//
-////        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-////        linearSlide2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-//
-//        int linearSlidePosition = 0;
-//        waitForStart();
-////        linearSlide.setPower(0.2);
-//        while (opModeIsActive()) {
-//            linearSlidePosition = linearSlide.getCurrentPosition();
-//            if (gamepad1.dpad_up) {
-//                telemetry.addData("Info", "Dpad Up");
-//                linearSlide.setVelocity(velocity);
-//                linearSlide2.setVelocity(velocity);
-//                linearSlidePosition+=deltaMovement;
-//                linearSlide2.setTargetPosition(linearSlidePosition);
-//                linearSlide.setTargetPosition(linearSlidePosition);
-//            }
-//            if (gamepad1.dpad_down) {
-//                telemetry.addData("Info", "Dpad Down");
-//                linearSlide.setVelocity(velocity);
-//                linearSlide2.setVelocity(velocity);
-//                linearSlidePosition-=deltaMovement;
-//                linearSlide2.setTargetPosition(linearSlidePosition);
-//                linearSlide.setTargetPosition(linearSlidePosition);
-//            }
-//
-//
-//            linearSlide.setVelocity(0);
-//            telemetry.addData("Out of the if statements, ", "asdf");
-//            telemetry.addData("Linear Slide Velocity", linearSlide.getVelocity());
-//            telemetry.addData("Linear Slide Position", linearSlidePosition);
-//            telemetry.update();
-//        }
-//    }
-//}
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+@TeleOp(group = "linearSlidesTest")
+@Config
+public class linearSlidesTest extends LinearOpMode {
+    public static int velocity = 1220;
+    public static int deltaMovement = 100;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        DcMotorEx linearSlide = hardwareMap.get(DcMotorEx.class, "linearSlide");
+        DcMotorEx linearSlide2 = hardwareMap.get(DcMotorEx.class, "linearSlide2");
+
+
+        linearSlide.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        linearSlide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        //weird thing since it is giving an error for needing to set a position before you start
+//        linearSlide.setTargetPosition(0);
+//        linearSlide.setTargetPosition(0);
+
+//        linearSlide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+//        linearSlide2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        int linearSlidePosition = 0;
+        waitForStart();
+//        linearSlide.setPower(0.2);
+        while (opModeIsActive()) {
+            linearSlidePosition = linearSlide.getCurrentPosition();
+            if (gamepad1.dpad_up) {
+                telemetry.addData("Info", "Dpad Up");
+                linearSlide.setVelocity(velocity);
+                linearSlide2.setVelocity(velocity);
+                linearSlidePosition+=deltaMovement;
+                linearSlide2.setTargetPosition(linearSlidePosition);
+                linearSlide.setTargetPosition(linearSlidePosition);
+            }
+            if (gamepad1.dpad_down) {
+                telemetry.addData("Info", "Dpad Down");
+                linearSlide.setVelocity(velocity);
+                linearSlide2.setVelocity(velocity);
+                linearSlidePosition-=deltaMovement;
+                linearSlide2.setTargetPosition(linearSlidePosition);
+                linearSlide.setTargetPosition(linearSlidePosition);
+            }
+
+
+            linearSlide.setVelocity(0);
+            telemetry.addData("Out of the if statements, ", "asdf");
+            telemetry.addData("Linear Slide Velocity", linearSlide.getVelocity());
+            telemetry.addData("Linear Slide Position", linearSlidePosition);
+            telemetry.update();
+        }
+    }
+}

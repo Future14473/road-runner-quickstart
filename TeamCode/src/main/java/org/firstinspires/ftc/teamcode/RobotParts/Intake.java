99c10c22f3840c3140c3edbfd4b78a4ddbe16569
kyleng <ng.kyle101@gmail.com>
2021-11-11 18:36:38 -0800

    General idea for intake, need to manually adjust servo positions first with mechanical

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Intake.java
@@ -1,17 +1,33 @@
 package org.firstinspires.ftc.teamcode.RobotParts;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
+@Config
 public class Intake {
     DcMotor noodles;
     Servo retracter;
+    Servo transfer;
+
+    public static double retractInPos = 0;
+    public static double retractOutPos = 1.0;
+    public static double transferIntakePos = 0;
+    public static double transferOutputPos = 1.0;
+
 
     public Intake(HardwareMap hardwareMap){
         noodles = hardwareMap.get(DcMotor.class, "noodles");
+        noodles.setDirection(DcMotorSimple.Direction.REVERSE);
+
         retracter = hardwareMap.get(Servo.class, "retracter");
+        retracter.setDirection(Servo.Direction.REVERSE);
+
+        transfer = hardwareMap.get(Servo.class, "transfer");
     }
+
     void setNoodlePower(double power){
         noodles.setPower(power);
     }
@@ -21,5 +37,26 @@ public class Intake {
     }
     public void outNoodles(){
         noodles.setPower(-1.0);
+    }
+
+    public void flipIn(){
+        retracter.setPosition(retractInPos);
+    }
+
+    public void flipOut(){
+        retracter.setPosition(retractOutPos);
+    }
+
+    //goes to the intake transfer position
+    public void transferIntake(){
+        transfer.setPosition(transferIntakePos);
+    }
+
+    public void transferOutake(){
+        transfer.setPosition(transferOutputPos);
+    }
+//    public void retract()
+    public double getRetracterPosition(){
+        return retracter.getPosition();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/Teleop.java
@@ -11,7 +11,35 @@ public class Teleop extends LinearOpMode {
         waitForStart();
 
         while (opModeIsActive()){
-            intake.setNoodlePower(gamepad1.right_trigger + gamepad1.left_trigger);
+            intake.setNoodlePower(gamepad1.right_trigger - gamepad1.left_trigger);
+
+            if (gamepad1.x){
+                intake.flipIn();
+                telemetry.addData("Retracter In Position ", Intake.retractInPos);
+                telemetry.addData("Flip Status", "in");
+            }
+
+            if (gamepad1.a) {
+                intake.flipOut();
+                telemetry.addData("Retracter In Position ", Intake.retractOutPos);
+                telemetry.addData("Flip Status", "out");
+            }
+
+            if (gamepad1.b) {
+                intake.transferIntake();
+                telemetry.addData("Transfer In Position ", Intake.transferIntakePos);
+                telemetry.addData("Transfer Status", "Intake Position");
+            }
+
+            if (gamepad1.y) {
+                intake.transferOutake();
+                telemetry.addData("Transfer In Position ", Intake.transferOutputPos);
+                telemetry.addData("Transfer Status", "Output Position");
+            }
+
+
+            telemetry.addData("Retracter Position ", intake.getRetracterPosition());
+            telemetry.update();
         }
     }
 }

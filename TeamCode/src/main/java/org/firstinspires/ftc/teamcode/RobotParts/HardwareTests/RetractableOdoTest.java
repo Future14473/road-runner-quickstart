41ab33e975dc98cef06e73a29d5c2aa635f082d9
kyleng <ng.kyle101@gmail.com>
2021-11-24 12:19:36 -0800

    Good Retractable odo positions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/HardwareTests/RetractableOdoTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.RobotParts.HardwareTests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.RobotParts.Intake;
+import org.firstinspires.ftc.teamcode.RobotParts.Output;
+import org.firstinspires.ftc.teamcode.RobotParts.RetractableOdo;
+
+@TeleOp(group = "1 Teleop")
+public class RetractableOdoTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RetractableOdo retractableOdo = new RetractableOdo(hardwareMap);
+        waitForStart();
+
+        while (opModeIsActive()){
+
+
+            if (gamepad1.x){
+                retractableOdo.upOdo();
+                telemetry.addData("Retractable Odo Status ", "Up");
+            }
+            if (gamepad1.y){
+                retractableOdo.downOdo();
+                telemetry.addData("Retractable Odo Status ", "Down");
+            }
+
+            telemetry.addData("Retractable Odo Position Perp", retractableOdo.getPerpPos());
+            telemetry.addData("Retractable Odo Position Parallel", retractableOdo.getParallelPos());
+
+            telemetry.update();
+        }
+    }
+}

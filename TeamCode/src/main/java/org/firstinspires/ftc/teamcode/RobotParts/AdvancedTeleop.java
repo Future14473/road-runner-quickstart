0c88ea47c9929f99aae5d5ee3ddb1bce5964f82d
kyleng <ng.kyle101@gmail.com>
2021-12-04 18:47:36 -0800

    Place drivetrain in thread so you can still control the drivetrain while using cycler automations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/AdvancedTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/AdvancedTeleop.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode.RobotParts;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
@@ -19,6 +18,23 @@ public class AdvancedTeleop extends LinearOpMode {
 
         waitForStart();
         retractableOdo.upOdo();
+        //drivetrain in thread so that you can use the automated cycling and still control the drivetrain
+        new Thread( () -> {
+            while(opModeIsActive()){
+                //BOTTOM DRIVER____________________________________________________________
+                //In-Line if statement that slows down the drivetrain if right bumper pressed
+
+                drive.setWeightedDrivePower(
+                        new Pose2d(
+                                gamepad1.right_bumper ? -gamepad1.left_stick_y * 0.5 : -gamepad1.left_stick_y,
+                                gamepad1.right_bumper ? -gamepad1.left_stick_x * 0.8: -gamepad1.left_stick_x,
+                                gamepad1.right_bumper ? -gamepad1.right_stick_x * 0.5: -gamepad1.right_stick_x
+
+                        )
+                );
+            }
+        }).start();
+
         while (opModeIsActive()){
 
             //TOP DRIVER
@@ -49,23 +65,28 @@ public class AdvancedTeleop extends LinearOpMode {
                 duck.setRedSpeed();
                 telemetry.addData("Duck Status", "Red");
             }
+            duck.setSpeed();
 
             // CAPSTONE______________________________________________________________
-            if (gamepad2.dpad_up){
-                capstone.grab();
-            }
-            if (gamepad2.dpad_down) {
-                capstone.place();
+            if (gamepad2.dpad_left){
+                capstone.down();
+                telemetry.addData("Capstone Position", "grab");
             }
 
-            //BOTTOM DRIVER____________________________________________________________
-            drive.setWeightedDrivePower(
-                    new Pose2d(
-                            -gamepad1.left_stick_y,
-                            -gamepad1.left_stick_x,
-                            -gamepad1.right_stick_x
-                    )
-            );
+            if (gamepad2.dpad_up){
+                capstone.holdUp();
+                telemetry.addData("Capstone Position", "hold Up");
+            }
+
+            if (gamepad2.dpad_right){
+                capstone.preCap();
+                telemetry.addData("Capstone Position", "preCap");
+            }
+
+            if (gamepad2.dpad_down){
+                capstone.cap();
+                telemetry.addData("Capstone Position", "cap");
+            }
 
 
             telemetry.update();

a48dd5f7af9d8703fddd8dc2b3eab0e78612fed4
kyleng <ng.kyle101@gmail.com>
2021-12-04 16:36:25 -0800

    Capstone positions not bad but needs more tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/HardwareTests/CapstoneTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotParts/HardwareTests/CapstoneTest.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.RobotParts.Capstone;
-import org.firstinspires.ftc.teamcode.RobotParts.Intake;
 
 @TeleOp(group = "1 Teleop")
 public class CapstoneTest extends LinearOpMode {
@@ -15,15 +14,30 @@ public class CapstoneTest extends LinearOpMode {
 
         while (opModeIsActive()){
             if (gamepad2.x){
-                capstone.grab();
+                capstone.down();
                 telemetry.addData("Capstone Position", "grab");
             }
 
             if (gamepad2.y){
-                capstone.place();
+                capstone.highestLimit();
                 telemetry.addData("Capstone Position", "place");
             }
 
+            if (gamepad2.b){
+                capstone.cap();
+                telemetry.addData("Capstone Position", "cap");
+            }
+
+            if (gamepad2.a){
+                capstone.holdUp();
+                telemetry.addData("Capstone Position", "hold Up");
+            }
+
+
+            if (gamepad2.right_bumper){
+                capstone.preCap();
+                telemetry.addData("Capstone Position", "preCap");
+            }
 
 
             telemetry.update();

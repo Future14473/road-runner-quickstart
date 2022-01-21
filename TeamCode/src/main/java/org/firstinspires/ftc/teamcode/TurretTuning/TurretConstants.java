8a8dad637e959733e8db98df089ef5515c65f2f5
kyleng <ng.kyle101@gmail.com>
2022-01-20 21:37:11 -0800

    Corrected Gear Setup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTuning/TurretConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TurretTuning/TurretConstants.java
@@ -3,13 +3,14 @@ package org.firstinspires.ftc.teamcode.TurretTuning;
 public class TurretConstants {
     static double motorTeeth = 14;
     static double motorToBevelTeeth = 28;
-    static double bevelToTurretTeeth = 30;
+    static double normalGearToTurretTeeth = 30;
     static double turretTeeth = 225;
     static double maxRotationDegrees = 360;
 
     static double LAZY_SUSAN_TICKS_PER_REVOLUTION = 103.8;
 
-    static double MOTOR_ROTATIONS_PER_TURRET_ROTATIONS = (turretTeeth/bevelToTurretTeeth) * (bevelToTurretTeeth/motorToBevelTeeth)*(motorToBevelTeeth/motorTeeth);
+    static double MOTOR_ROTATIONS_PER_TURRET_ROTATIONS =
+            (turretTeeth/ normalGearToTurretTeeth) *(motorToBevelTeeth/motorTeeth);
             //(turretTeeth / (bevelToTurretTeeth / (motorToBevelTeeth / motorTeeth)));
 
     // math should be output/input

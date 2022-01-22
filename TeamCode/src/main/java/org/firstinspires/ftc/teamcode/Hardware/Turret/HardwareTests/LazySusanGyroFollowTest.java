5f1748f4ff93a61c5c7dc8468522d990f0b657b2
kyleng <ng.kyle101@gmail.com>
2022-01-22 14:50:55 -0800

    init

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Turret/HardwareTests/LazySusanGyroFollowTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Turret/HardwareTests/LazySusanGyroFollowTest.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.Hardware.Turret.HardwareTests;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.Hardware.IMU.IMU;
 import org.firstinspires.ftc.teamcode.Hardware.Turret.LazySusan;
 
 @TeleOp
@@ -10,8 +11,9 @@ public class LazySusanGyroFollowTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         LazySusan lazySusan = new LazySusan(hardwareMap);
+        IMU imu = new IMU(hardwareMap);
         waitForStart();
-        lazySusan.rotateToDegrees(90);
+        lazySusan.rotateToDegrees(imu.getHeading());
         while (opModeIsActive()){
             telemetry.addData("Turret Position", lazySusan.getTicks());
             telemetry.addData("Turret Degrees", lazySusan.getDegrees());

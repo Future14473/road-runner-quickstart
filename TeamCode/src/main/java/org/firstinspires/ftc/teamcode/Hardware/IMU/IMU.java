e6a777d1c0121b4680379eb29abeb886de1bcac2
kyleng <ng.kyle101@gmail.com>
2022-01-22 14:49:20 -0800

    Add IMU from Ultimate Goal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/IMU/IMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/IMU/IMU.java
@@ -3,14 +3,42 @@ package org.firstinspires.ftc.teamcode.Hardware.IMU;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
 
-public class IMU {
-    BNO055IMU imu;
-    public IMU(HardwareMap hardwareMap){
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+public class IMU  {
+    public BNO055IMU imu;
+
+    // added to raw heading in case we need to reset the zero point
+    public double headingOffset = 0;
+
+    public IMU (HardwareMap hardwareMap){
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+
+        parameters.mode = BNO055IMU.SensorMode.IMU;
         parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+
+        parameters.loggingEnabled = false;
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
         imu.initialize(parameters);
+        // imu.startAccelerationIntegration(new Position(), new Velocity(), 1000);
+    }
+
+    public double getHeading(){
+        Orientation angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.RADIANS);
+        return angles.firstAngle + headingOffset;
+    }
+
+    public void thisHeadingIsActually(double oldHeading, double newHeading){
+        headingOffset += newHeading - oldHeading;
     }
 
 }

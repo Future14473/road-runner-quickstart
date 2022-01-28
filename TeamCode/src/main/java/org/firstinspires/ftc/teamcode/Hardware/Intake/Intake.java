3a27a02bdab0d3b6211c1cc6e5d46132caab1540
kyleng <ng.kyle101@gmail.com>
2022-01-27 20:09:02 -0800

    Intake motor reversed

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Intake/Intake.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.Hardware.Intake;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Intake {
@@ -8,6 +9,7 @@ public class Intake {
 
     public Intake(HardwareMap hardwareMap) {
         intake = hardwareMap.get(DcMotor.class, "intake");
+        intake.setDirection(DcMotorSimple.Direction.REVERSE);
     }
     public void in(){
         intake.setPower(1.0);

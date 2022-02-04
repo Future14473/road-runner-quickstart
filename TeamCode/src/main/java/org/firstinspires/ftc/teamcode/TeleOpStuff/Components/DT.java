8f976ddc41020d830cecd78eccb5544492af1b96
ethan <ethantt2012@gmail.com>
2022-02-04 09:15:03 -0800

    leik potato

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpStuff/Components/DT.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.TeleOpStuff.Components;
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+public class DT {
+
+    DcMotorEx rightFront;
+    DcMotorEx rightBack;
+    DcMotorEx leftFront;
+    DcMotorEx leftBack;
+
+    public DT(HardwareMap hardwareMap, LinearOpMode opMode){
+        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        rightBack = hardwareMap.get(DcMotorEx.class, "rightBack");
+        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
+        leftBack = hardwareMap.get(DcMotorEx.class, "leftBack");
+        rightFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFront.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBack.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpStuff/TeleOpE.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode.TeleOpStuff;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name = "AAA Teleop", group = "Teleop")
+public class TeleOpE extends LinearOpMode {
+    public void runOpMode() throws InterruptedException{
+
+    }
+}
+

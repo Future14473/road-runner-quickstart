ac09bc9e3397a0e0fdb71f46f359e946cf8abab6
kyleng <ng.kyle101@gmail.com>
2022-02-05 22:23:50 -0800

    Add All Mechanisms

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Opmodes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Opmodes/TeleOp.java
@@ -7,6 +7,8 @@ import org.firstinspires.ftc.teamcode.Hardware.Outtake.Outtake;
 import org.firstinspires.ftc.teamcode.Hardware.Outtake.Slides;
 
 import org.firstinspires.ftc.teamcode.Hardware.Outtake.Linkages;
+import org.firstinspires.ftc.teamcode.Hardware.Turret.LazySusan;
+import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp
 public class TeleOp extends LinearOpMode {
@@ -14,27 +16,38 @@ public class TeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         Outtake outtake = new Outtake(hardwareMap);
         Intake intake = new Intake(hardwareMap);
+        LazySusan lazySusan = new LazySusan(hardwareMap);
+        SampleTankDrive tankDrive = new SampleTankDrive(hardwareMap);
         waitForStart();
 
         while (opModeIsActive()) {
 
-            if (gamepad1.x) {
+            if (gamepad1.dpad_up) {
                 outtake.slides.extendHigh();
             }
-            if (gamepad1.a) {
+            if (gamepad1.dpad_down) {
                 outtake.slides.retract();
             }
-            if (gamepad1.y) {
+            if (gamepad1.dpad_right) {
                 outtake.linkages.extend();
             }
-            if (gamepad1.b) {
+            if (gamepad1.dpad_left) { // make retract and down all in one method later
                 outtake.linkages.retract();
             }
-            if (gamepad1.dpad_up) {
+            if (gamepad1.x) {
                 outtake.linkages.DumperOut();
             }
-            if (gamepad1.dpad_down) {
+            if (gamepad1.y) {
                 outtake.linkages.DumperIn();
+            }
+            if(gamepad1.b){
+                lazySusan.rotateToDegrees(90);
+            }
+            if(gamepad1.a){
+                lazySusan.rotateToDegrees(-90);
+            }
+            if(gamepad1.right_bumper){
+                lazySusan.rotateToDegrees(0);
             }
 
            if (gamepad1.right_trigger > 0){
@@ -44,6 +57,8 @@ public class TeleOp extends LinearOpMode {
             } else{
                 intake.stop();
             }
+
+           tankDrive.setPowerDir(gamepad1.left_stick_y, gamepad1.left_stick_x);
         }
     }
 }

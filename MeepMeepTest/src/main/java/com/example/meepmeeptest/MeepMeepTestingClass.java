c00dbbcd8807a25002e562319f7a46c753b11060
kyleng <ng.kyle101@gmail.com>
2022-02-11 17:05:24 -0800

    Test position of 1 cycle

--- MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTestingClass.java
+++ MeepMeepTest/src/main/java/com/example/meepmeeptest/MeepMeepTestingClass.java
@@ -1,6 +1,7 @@
 package com.example.meepmeeptest;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
@@ -14,15 +15,8 @@ public class MeepMeepTestingClass {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
-                                .forward(30)
-                                .turn(Math.toRadians(90))
+                        drive.trajectorySequenceBuilder(new Pose2d(27.5, 67.2, 180))
+                                .splineTo(new Vector2d(-0.5, 30), Math.toRadians(195))
                                 .build()
                 );
 

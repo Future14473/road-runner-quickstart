e6e357ca9df4e15c6da1cb289ad0aaaccc6cbb8f
kyleng <ng.kyle101@gmail.com>
2022-02-13 00:53:49 -0800

    Finished turn track width and localizationtest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TurnTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/TurnTest.java
@@ -13,7 +13,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
 @Config
 @Autonomous(group = "drive")
 public class TurnTest extends LinearOpMode {
-    public static double ANGLE = 90; // deg
+    public static double ANGLE = 180; // deg
 
     @Override
     public void runOpMode() throws InterruptedException {

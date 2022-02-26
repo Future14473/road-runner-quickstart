0b9ef5235e682133da5f5db58b1a92484fbce627
kyleng <ng.kyle101@gmail.com>
2022-02-26 10:30:58 -0800

    Disable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Opmodes/OLDDONOTUSE/AutoBlueDuck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/Opmodes/OLDDONOTUSE/AutoBlueDuck.java
@@ -1,10 +1,11 @@
-package org.firstinspires.ftc.teamcode.Hardware.Opmodes;
+package org.firstinspires.ftc.teamcode.Hardware.Opmodes.OLDDONOTUSE;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.Hardware.Duck.Duck;
@@ -15,6 +16,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
 
 @Autonomous
 @Config
+@Disabled
 public class AutoBlueDuck extends LinearOpMode {
     public static double preloadX = -20, preloadY = 54.5, preloadH = 290,
                             duckX = -60, duckY = 66.5, duckH = 180,
